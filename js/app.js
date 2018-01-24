1280526ca2c70710e011146633757c2235a26a61
Esraa Qandeel <esraamedhatesamir@gmail.com>
2018-01-24 17:58:46 +0200

    connecting Gmaps and KO

--- js/app.js
+++ js/app.js
@@ -1,62 +1,115 @@
 /* Model */
 var initData = [
   {
-    title: 'Bronze Horseman',
-    location: {lat: 59.936378, lng: 30.30223}
+    title: 'Museum of Islamic Art',
+    location: {lat: 30.0444, lng: 31.2524},
+    id: 1
   },
   {
-    title: 'Saint Isaac\'s Cathedral',
-    location: {lat: 59.933905, lng: 30.306485}
+    title: 'Cairo Citadel',
+    location: {lat: 30.0299, lng: 31.2611},
+    id: 2
   },
   {
-    title: 'Church of the Savior on Blood',
-    location: {lat: 59.940100, lng: 30.3289}
+    title: 'Mosque-Madrassa of Sultan Hassan',
+    location: {lat: 30.0323, lng: 31.2562},
+    id: 3
   },
   {
-    title: 'Hermitage Museum',
-    location: {lat: 59.939832, lng: 30.31456}
+    title: 'The Hanging Church',
+    location: {lat: 30.0053, lng: 31.2302},
+    id: 4
   },
   {
-    title: 'Rostral Columns',
-    location: {lat: 59.944682, lng: 30.304971}
+    title: 'Maimonides Synagogue',
+    location: {lat: 30.0509, lng: 31.2626},
+    id: 5
   }
 ];
 
-      var map;
-      var markers = [];
+//       var map;
+//       var markers = [];
 
-      function initMap() {
-        // Constructor creates a new map - only center and zoom are required.
+//       function initMap() {
+//         // Constructor creates a new map - only center and zoom are required.
+//         map = new google.maps.Map(document.getElementById('map'), {
+//           center: { lat: 59.942803, lng: 30.324841 },
+//           zoom: 13,
+//           mapTypeControl: false
+//         });
+
+//         ko.applyBindings(new ViewModel())
+
+// }
+
+
+function initMap() {
+
+
+    function setMap(callback) {
         map = new google.maps.Map(document.getElementById('map'), {
-          center: { lat: 59.942803, lng: 30.324841 },
-          zoom: 13,
-          mapTypeControl: false
+            center:{ lat: 30.0444, lng: 31.2357 },
+            zoom: 13
         });
 
-        ko.applyBindings(new ViewModel())
+        callback();
+    }
 
-        var largeInfowindow = new google.maps.InfoWindow();
+    // call setMap and, after map is rendered, call Knockout bindings via new MyApp()
+    setMap(function() {
+        ko.applyBindings(new ViewModel());
+    });
+}
+/* ViewModel */
+var ViewModel = function() {
+    var self = this;
+    var markers = [];
+    var largeInfowindow = new google.maps.InfoWindow();
+    var bounds = new google.maps.LatLngBounds();
+   
+    self.query = ko.observable('');
+    self.showMarker = function(clickeditem){
+        // console.log(self.filteredList().length);
+        for (var i = 0; i < markers.length; i++) {
+          if(markers[i].id == clickeditem.id ){
+            populateInfoWindow(markers[i], largeInfowindow);
+          }
+     }
+}
+    self.filteredList = ko.computed(function() {
+        var search = self.query().toLowerCase();
+        return ko.utils.arrayFilter(initData, function(item) {
+            return item.title.toLowerCase().indexOf(search) >= 0;
+        });
+});
+       
+
         // The following group uses the location array to create an array of markers on initialize.
-        for (var i = 0; i < ViewModel.List.length; i++) {
+        for (var i = 0; i < self.filteredList().length; i++) {
 
           // Get the position from the location array.
-          var position = list[i].location;
-          var title = list[i].title;
+          var position = self.filteredList()[i].location;
+          var title = self.filteredList()[i].title;
+          var id = self.filteredList()[i].id;
           // Create a marker per location, and put into markers array.
            var marker = new google.maps.Marker({
             map:map,
             position: position,
             title: title,
             animation: google.maps.Animation.DROP,
-            id: i
+            id: id
           });
           // Push the marker to our array of markers.
           markers.push(marker);
           // Create an onclick event to open an infowindow at each marker.
           marker.addListener('click', function() {
             populateInfoWindow(this, largeInfowindow);
+
           });
+          bounds.extend(markers[i].position);
         }
+                // Extend the boundaries of the map for each marker
+        map.fitBounds(bounds);
       // This function populates the infowindow when the marker is clicked. We'll only allow
       // one infowindow which will open at the marker that is clicked, and populate based
       // on that markers position.
@@ -64,8 +117,12 @@ var initData = [
         // Check to make sure the infowindow is not already opened on this marker.
         if (infowindow.marker != marker) {
           infowindow.marker = marker;
-          infowindow.setContent('<div>' + marker.title + '</div>');
+          infowindow.setContent('<div id='+marker.id+'>' + marker.title + '</div>');
           infowindow.open(map, marker);
+          marker.setAnimation(google.maps.Animation.BOUNCE);
+          setTimeout(function() {
+          marker.setAnimation(null);
+             }, 1400);
           // Make sure the marker property is cleared if the infowindow is closed.
           infowindow.addListener('closeclick', function() {
             infowindow.marker = null;
@@ -73,19 +130,24 @@ var initData = [
         }
       }
 
-      }
-/* ViewModel */
-var ViewModel = function() {
-    var self = this;
-    self.query = ko.observable('sa');
-    self.alertHi = function(clickeditem){
+      self.locationForFilteredList = function (){
 
-        console.log(clickeditem)
-    }
-    self.List = ko.computed(function() {
-        var search = self.query().toLowerCase();
-        return ko.utils.arrayFilter(initData, function(item) {
-            return item.title.toLowerCase().indexOf(search) >= 0;
-        });
-});
+
+        for (var i = 0; i < markers.length; i++) {
+
+            markers[i].setMap(null);
+            for (var j = 0; j< self.filteredList().length; j++){
+
+               if (self.filteredList()[j].id == markers[i].id){
+                     markers[i].setMap(map);
+
+                    console.log('MATCH !!! for marker'+ markers[i].id + 'for list'+self.filteredList()[j].id)
+                }
+            }
+          
+        }
+
+      }
 }
+
+
