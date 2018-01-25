f39137973b602b40be78eb1e3b0ea5ce8a7ddbff
Esraa Qandeel <esraamedhatesamir@gmail.com>
2018-01-25 11:33:38 +0200

    adding README

---
+++ README.md
@@ -0,0 +1,45 @@
+# Project Overview
+
+>You will develop a single page application featuring a map of your neighborhood or a neighborhood you would like to visit. You will then add 
+>functionality to this map including highlighted locations, third-party data about those locations and various ways to browse the content.
+
+
+### Used in the Project 
+
+1- Google Maps APS 
+
+2- KnochoutJS 
+
+3- Wikipedia API 
+
+### Requiremnets 
+
+1- Inerent access 
+
+2- Browser
+
+
+
+## Running the App
+
+1- just clone the repo 
+
+2- run index.html on your favourite browser 
+
+3- Enjoy your tour in Egypt! 
+
+
+## Resources 
+
+- Credit goes to the owner of this [repo](https://github.com/jennikins813/Neighborhood-Map/blob/master/js/main.js) for introducing me to JS callback functions
+
+- [Understanding callbacks](https://www.youtube.com/watch?v=pTbSfCT42_M)
+
+- [Marker Animation](https://developers.google.com/maps/documentation/javascript/examples/marker-animations)
+
+- [Template binding](http://knockoutjs.com/documentation/template-binding.html)
+
+- [Replace dependent Observables](http://knockoutjs.com/documentation/dependentObservables.html)
+
+- [Filter The list](https://stackoverflow.com/questions/29551997/knockout-search-filter)
+
--- js/app.js
+++ js/app.js
@@ -1,30 +1,29 @@
 /* Model */
-var initData = [
-  {
-    title: 'Museum of Islamic Art',
-    location: {lat: 30.0444, lng: 31.2524},
-    id: 1
-  },
-  {
-    title: 'Cairo Citadel',
-    location: {lat: 30.0299, lng: 31.2611},
-    id: 2
-  },
-  {
-    title: 'Mosque-Madrassa of Sultan Hassan',
-    location: {lat: 30.0323, lng: 31.2562},
-    id: 3
-  },
-  {
-    title: 'The Hanging Church',
-    location: {lat: 30.0053, lng: 31.2302},
-    id: 4
-  },
-  {
-    title: 'Maimonides Synagogue',
-    location: {lat: 30.0509, lng: 31.2626},
-    id: 5
-  }
+var initData = [{
+        title: 'Museum of Islamic Art',
+        location: { lat: 30.0444, lng: 31.2524 },
+        id: 1
+    },
+    {
+        title: 'Cairo Citadel',
+        location: { lat: 30.0299, lng: 31.2611 },
+        id: 2
+    },
+    {
+        title: 'Mosque-Madrassa of Sultan Hassan',
+        location: { lat: 30.0323, lng: 31.2562 },
+        id: 3
+    },
+    {
+        title: 'The Hanging Church',
+        location: { lat: 30.0053, lng: 31.2302 },
+        id: 4
+    },
+    {
+        title: 'Maimonides Synagogue',
+        location: { lat: 30.0509, lng: 31.2626 },
+        id: 5
+    }
 ];
 
 
@@ -34,18 +33,20 @@ function initMap() {
 
     function setMap(callback) {
         map = new google.maps.Map(document.getElementById('map'), {
-            center:{ lat: 30.0444, lng: 31.2357 },
+            center: { lat: 30.0444, lng: 31.2357 },
             zoom: 13
         });
 
         callback();
     }
 
-    // call setMap and, after map is rendered, call Knockout bindings via new MyApp()
+    // call setMap and, after map is rendered, call Knockout bindings via new ViewModel()
     setMap(function() {
         ko.applyBindings(new ViewModel());
     });
 }
+
+
 /* ViewModel */
 var ViewModel = function() {
     var self = this;
@@ -53,103 +54,99 @@ var ViewModel = function() {
     var message;
     var largeInfowindow = new google.maps.InfoWindow();
     var bounds = new google.maps.LatLngBounds();
-   
+
     self.query = ko.observable('');
-    self.showMarker = function(clickeditem){
-        // console.log(self.filteredList().length);
+    self.showMarker = function(clickeditem) {
         for (var i = 0; i < markers.length; i++) {
-          if(markers[i].id == clickeditem.id ){
-            populateInfoWindow(markers[i], largeInfowindow);
-          }
-     }
-}
+            if (markers[i].id == clickeditem.id) {
+                populateInfoWindow(markers[i], largeInfowindow);
+            }
+        }
+    }
     self.filteredList = ko.computed(function() {
         var search = self.query().toLowerCase();
         return ko.utils.arrayFilter(initData, function(item) {
             return item.title.toLowerCase().indexOf(search) >= 0;
         });
-});
-       
+    });
 
-        // The following group uses the location array to create an array of markers on initialize.
-        for (var i = 0; i < self.filteredList().length; i++) {
 
-          // Get the position from the location array.
-          var position = self.filteredList()[i].location;
-          var title = self.filteredList()[i].title;
-          var id = self.filteredList()[i].id;
-          // Create a marker per location, and put into markers array.
-           var marker = new google.maps.Marker({
-            map:map,
+    // The following group uses the filteredList array to create an array of markers on initialize.
+    for (var i = 0; i < self.filteredList().length; i++) {
+
+        // Get the position from the location array.
+        var position = self.filteredList()[i].location;
+        var title = self.filteredList()[i].title;
+        var id = self.filteredList()[i].id;
+        // Create a marker per location, and put into markers array.
+        var marker = new google.maps.Marker({
+            map: map,
             position: position,
             title: title,
             animation: google.maps.Animation.DROP,
             id: id
-          });
-          // Push the marker to our array of markers.
-          markers.push(marker);
-          // Create an onclick event to open an infowindow at each marker.
-          marker.addListener('click', function() {
+        });
+        // Push the marker to our array of markers.
+        markers.push(marker);
+        // Create an onclick event to open an infowindow at each marker.
+        marker.addListener('click', function() {
             populateInfoWindow(this, largeInfowindow);
 
-          });
-          bounds.extend(markers[i].position);
-        }
-                // Extend the boundaries of the map for each marker
-        map.fitBounds(bounds);
-      // This function populates the infowindow when the marker is clicked. We'll only allow
-      // one infowindow which will open at the marker that is clicked, and populate based
-      // on that markers position.
-      function populateInfoWindow(marker, infowindow) {
+        });
+        bounds.extend(markers[i].position);
+    }
+    // Extend the boundaries of the map for each marker
+    map.fitBounds(bounds);
+    // This function populates the infowindow when the marker is clicked. We'll only allow
+    // one infowindow which will open at the marker that is clicked, and populate based
+    // on that markers position.
+    function populateInfoWindow(marker, infowindow) {
         // Check to make sure the infowindow is not already opened on this marker.
         if (infowindow.marker != marker) {
-          infowindow.marker = marker;
-          var wikiRequestTimeout = setTimeout(function(){
+            infowindow.marker = marker;
+            var wikiRequestTimeout = setTimeout(function() {
+               infowindow.setContent('<div id=' + marker.id + '>' + marker.title + '</div><p>wikipedia failed,Try again later!</p>');
+            }, 2000)
+            var wikiURL = "https://en.wikipedia.org/w/api.php?action=opensearch&search=" + marker.title + "&format=json&callback=wikiCallback";
+            $.ajax({
+                url: wikiURL,
+                dataType: "jsonp",
+                success: function(response) {
+                    var list = response[1];
+                    var link = "https://en.wikipedia.org/wiki/" + list[0];
+                    infowindow.setContent('<div id=' + marker.id + '>' + marker.title + '</div><br/><a href="' + link + '" target="_blank">' +
+                        'visit wikipedia for more info!</h1>');
+                    clearTimeout(wikiRequestTimeout);
+                }
+            });
 
-           infowindow.setContent('<div id='+marker.id+'>' + marker.title + '</div><p>wikipedia failed,Try again later!</p>');
-            },2000)
-          var wikiURL = "https://en.wikipedia.org/w/api.php?action=opensearch&search="+marker.title+"&format=json&callback=wikiCallback";
-          $.ajax({
-            url: wikiURL,
-            dataType: "jsonp",
-            success: function( response ){
-            var list = response[1];
-            var link = "https://en.wikipedia.org/wiki/"+list[0];
-            infowindow.setContent('<div id='+marker.id+'>' + marker.title + '</div><br/><a href="'+link+'" target="_blank">read more about this place!</h1>');
-            clearTimeout(wikiRequestTimeout);
-         }
-        });
-          
-          infowindow.open(map, marker);
-          marker.setAnimation(google.maps.Animation.BOUNCE);
-          setTimeout(function() {
-          marker.setAnimation(null);
-             }, 1400);
-          // Make sure the marker property is cleared if the infowindow is closed.
-          infowindow.addListener('closeclick', function() {
-            infowindow.marker = null;
-          });
+            infowindow.open(map, marker);
+            marker.setAnimation(google.maps.Animation.BOUNCE);
+            setTimeout(function() {
+                marker.setAnimation(null);
+            }, 1400);
+            // Make sure the marker property is cleared if the infowindow is closed.
+            infowindow.addListener('closeclick', function() {
+                infowindow.marker = null;
+            });
         }
-      }
+    }
 
-      self.locationForFilteredList = function (){
-
+    self.locationForFilteredList = function() {
 
         for (var i = 0; i < markers.length; i++) {
 
             markers[i].setMap(null);
-            for (var j = 0; j< self.filteredList().length; j++){
+            for (var j = 0; j < self.filteredList().length; j++) {
 
-               if (self.filteredList()[j].id == markers[i].id){
-                     markers[i].setMap(map);
+                if (self.filteredList()[j].id == markers[i].id) {
+                    markers[i].setMap(map);
 
-                    console.log('MATCH !!! for marker'+ markers[i].id + 'for list'+self.filteredList()[j].id)
+                    console.log('MATCH !!! for marker' + markers[i].id + 'for list' + self.filteredList()[j].id)
                 }
             }
-          
+
         }
 
-      }
+    }
 }
-
-
